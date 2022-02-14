8ba9ca01bed952d1db07f712975d1fc2423983af
Azusena Arroyo <azusenaarroyo@Azusenas-MBP.lan>
2022-02-13 19:18:18 -0600

    deliverable 3

--- R_Analysis_MechaCar/MechaCarChallenge.RScript.R
+++ R_Analysis_MechaCar/MechaCarChallenge.RScript.R
@@ -8,3 +8,13 @@ summary(lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD, vehicl
 Suspension_coil <- read.csv('Suspension_Coil.csv', stringsAsFactors = F) #read in dataset
 total_summary <- Suspension_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance= var(PSI), SD= sd(PSI)) #create a summary data frame
 lot_summary <- Suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance= var(PSI), SD= sd(PSI)) #create a summary data frame grouped by manufacturing lot
+
+#Deliverable 3
+t.test(Suspension_coil$PSI, mu=1500) # determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch
+Lot1 = subset(Suspension_coil, Manufacturing_Lot=='Lot1')
+Lot2 = subset(Suspension_coil, Manufacturing_Lot=='Lot2')
+Lot3 = subset(Suspension_coil, Manufacturing_Lot=='Lot3')
+
+t.test(Lot1$PSI, mu = 1500)#determine if the PSI across each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch
+t.test(Lot2$PSI, mu = 1500) 
+t.test(Lot3$PSI, mu = 1500) 
